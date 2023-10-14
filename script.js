a9a463ecad681a508ce051a33d9e0751776c1e6f
Vishal Prabhu <prabhuvishal2018@gmail.com>
2023-10-14 23:42:14 +0530

    first commit

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ const github=document.querySelector('#github');
   
     function sendMail(){
     // smtpjs.com 
-
+//elastic email 
  let name= document.getElementById("name").value;
  let email=document.getElementById("email").value;
  let message=document.getElementById("message").value;
